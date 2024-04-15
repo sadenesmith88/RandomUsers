508677bd92cd9618c92e90880f9bc7912c87af41
Sade Nesmith <sadework21@gmail.com>
2024-04-15 15:30:19 -0400

    initial commit

---
+++ randomUsers/randomUsers/randomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomUsersApp.swift
+//  randomUsers
+//
+//  Created by sade on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct randomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
